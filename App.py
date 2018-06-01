d6ec1c96a33578436f929dfb4b1580d49ea192cb
Empiah <phippsoli@hotmail.com>
2018-06-01 13:17:49 +0100

    apply updates to how teams are selected

--- App.py
+++ App.py
@@ -14,7 +14,8 @@ df['year'] = df['date'].dt.year
 df = df[df['date'].dt.year >= 1975]
 
 available_indicators_home = np.sort(df['home_team'].unique())
-available_indicators_away = np.sort(df['away_team'].unique())
+available_indicators_away = ['Home', 'Away']
+#available_indicators_away = np.sort(df['away_team'].unique())
 
 app.layout = html.Div([
         html.Div([
@@ -26,16 +27,16 @@ app.layout = html.Div([
                     value='England'
                 ),
             ],
-            style={'width': '49%', 'display': 'inline-block'}),
+            style={'width': '75%', 'display': 'inline-block'}),
 
             html.Div([
                 dcc.Dropdown(
                     id='yaxis-column',
                     options=[{'label': i, 'value': i} for i in available_indicators_away],
-                    value='Italy'
+                    value='Home'
                 ),
             ],
-            style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+            style={'width': '23%', 'float': 'right', 'display': 'inline-block'})
         ], style={
             'borderBottom': 'thin lightgrey solid',
             'backgroundColor': 'rgb(250, 250, 250)',
@@ -69,25 +70,40 @@ def update_graph(xaxis_column_name, yaxis_column_name,
                  year_value):
     dff = df[df['year'] == year_value]
 
+    if yaxis_column_name == 'Home':
+        goal1 = dff[dff['home_team'] == xaxis_column_name]['home_score']
+        goal2 = dff.loc[dff['home_team'] == xaxis_column_name, 'away_score']
+        name = dff[dff['home_team'] == xaxis_column_name]['away_team']
+        custom = dff[dff['home_team'] == yaxis_column_name]['home_team']
+
+    else:
+        goal1 = dff[dff['away_team'] == xaxis_column_name]['away_score']
+        goal2 = dff.loc[dff['away_team'] == xaxis_column_name, 'home_score']
+        name = dff[dff['away_team'] == xaxis_column_name]['home_team']
+        custom = dff[dff['away_team'] == yaxis_column_name]['away_team']
+
+
     return {
         'data': [go.Scatter(
-            x=dff[dff['home_team'] == xaxis_column_name]['home_score'],
-            y=dff[dff['away_team'] == yaxis_column_name]['away_score'],
-            text=dff[dff['away_team'] == yaxis_column_name]['date'],
-            customdata=dff[dff['away_team'] == yaxis_column_name]['date'],
+            x=goal1,
+            y=goal2,
+            text=name,
+            customdata=custom,
             mode='markers',
-            marker={
-                'size': 15,
-                'opacity': 0.5,
-                'line': {'width': 0.5, 'color': 'white'}
-            }
+            marker=dict(
+                size = 15,
+                opacity = 0.5,
+                color = 'blue',
+                line = dict(width = 0.5, color = 'black'
+                )
+            )
         )],
         'layout': go.Layout(
             xaxis={
                 'title': xaxis_column_name,
             },
             yaxis={
-                'title': yaxis_column_name
+                'title': 'Opponent'
             },
             margin={'l': 40, 'b': 30, 't': 10, 'r': 0},
             height=450,
